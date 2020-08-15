8d7c7e725f1df2dd6ceebdbd5d680b746e83cb5c
Henriqueto <69655585+Henriqueto@users.noreply.github.com>
2020-08-15 15:52:37 -0300

    Adicionando Scripts Player

---
+++ 222/Assets/Scripts/GameController.cs
@@ -0,0 +1,290 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{    
+    private Camera cam;
+
+    public Transform playertransform;
+
+    public Transform tirotransform;
+
+    private Player Player;
+
+    private Inimigo Inimigo;
+
+    public float velocidadeCam;
+    public Transform LimiteCamEsq, LimiteCamDir, LimiteCamCima, LimiteCamBaixo;
+
+    public Text segundostxt;
+    public float segundos = 15;
+
+    [Header("Audio")]
+    public AudioSource sfxsource;
+    public AudioSource musicasource;
+
+    public AudioClip sfxbotao;
+    public AudioClip sfxcolisaotiro;
+    public AudioClip sfxtempo1;
+    public AudioClip sfxtempo2;
+    public AudioClip sfxpassoninja;
+    public AudioClip sfxvezjogar;
+    public AudioClip sfxkunay;
+    public AudioClip[] sfxpasso;
+
+
+    public GameObject musicaoff;
+
+    public bool pausatempo = true;
+
+    public bool vezplayer = true;
+    public bool vezinimigo = false;
+
+    public bool tocou3 = true;
+    public bool tocou2 = false;
+    public bool tocou1 = false;
+
+    public bool tocavez;
+
+    private CameraTouch CameraTouch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        Player = FindObjectOfType(typeof(Player)) as Player;
+
+        Inimigo = FindObjectOfType(typeof(Inimigo)) as Inimigo;
+        CameraTouch = FindObjectOfType(typeof(CameraTouch)) as CameraTouch;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentstate == gamestate.TITULO)
+        {
+            musicaoff.SetActive(false);
+        }
+        else
+        {
+            musicaoff.SetActive(true);
+        }
+
+        if (currentstate == gamestate.TITULO && CrossPlatformInputManager.GetButtonDown("botaoinicio"))
+        {
+            Playsfx(sfxbotao, 0.5f);
+            currentstate = gamestate.GAMEPLAY;
+            paineltitulo.SetActive(false);
+            pausatempo = false;
+        }
+        else if (currentstate == gamestate.FIMJOGO && CrossPlatformInputManager.GetButtonDown("botaoreinicia"))
+        {
+            Playsfx(sfxbotao, 0.5f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            pausatempo = true;
+        }
+
+        if (segundos <= 15 && pausatempo == false)
+        {
+            segundos -= Time.deltaTime;
+            segundostxt.text = segundos.ToString("F0");
+
+        }
+
+        if (segundos < 4 && tocou3 == true) //se segundos for 3
+        {
+            tocou3 = false;
+            Playsfx(sfxtempo1, 1f);
+            tocou2 = true;
+
+        }
+        if (segundos < 3 && tocou2 == true) //se segundos for 2
+        {
+            tocou2 = false;
+            Playsfx(sfxtempo2, 1f);
+            tocou1 = true;
+        }
+        if (segundos < 2 && tocou1 == true) //se segundos for 1
+        {
+            tocou1 = false;
+            Playsfx(sfxtempo1, 1f);
+
+        }
+
+        if (segundos < 0) //se segundos for 0
+        {
+            segundos = 15;
+            segundostxt.text = segundos.ToString("F0");
+            tocou3 = true;
+
+            if (vezinimigo == false)
+            {
+                vezinimigo = true;
+                vezplayer = false;
+                tocavez = false;
+                Player.podeatacar = false;
+            }
+            else
+            {
+                vezinimigo = false;
+                vezplayer = true;
+                tocavez = true;
+            }
+
+        }
+
+        if (vezplayer == true && tocavez == true)
+        {
+            Playsfx(sfxvezjogar, 0.9f);
+            tocavez = false;
+        }
+
+        if (vezinimigo == true || Inimigo.inimigovaiatacar == true)
+        {
+            Player.velocidade = 0;
+        }
+
+        if (vezplayer == true)
+        {
+            Player.velocidade = 2;
+        }
+
+    }
+
+    void LateUpdate()
+    {
+        if (tirotransform != null)
+        {
+            if (Player.vaiatacar == true && Player.podeatacar == true && CameraTouch.tocounatela == false) //camera do tiro player
+            {
+
+                float posCamArmaX = tirotransform.position.x;
+                float posCamArmaY = tirotransform.position.y;
+
+                if (cam.transform.position.x < LimiteCamEsq.position.x && tirotransform.transform.position.x < LimiteCamEsq.position.x)
+                {
+                    posCamArmaX = LimiteCamEsq.position.x;
+                }
+                else if (cam.transform.position.x > LimiteCamDir.position.x && tirotransform.transform.position.x > LimiteCamDir.position.x)
+                {
+                    posCamArmaX = LimiteCamDir.position.x;
+                }
+
+                if (cam.transform.position.y < LimiteCamBaixo.position.y && tirotransform.transform.position.y < LimiteCamBaixo.position.y)
+                {
+                    posCamArmaY = LimiteCamBaixo.position.y;
+                }
+                else if (cam.transform.position.y > LimiteCamCima.position.y && tirotransform.transform.position.y > LimiteCamCima.position.y)
+                {
+                    posCamArmaY = LimiteCamCima.position.y;
+                }
+
+                Vector3 CamArma = new Vector3(posCamArmaX, posCamArmaY, cam.transform.position.z);
+                cam.transform.position = Vector3.Lerp(cam.transform.position, CamArma, velocidadeCam * Time.deltaTime);
+
+            }
+        }
+
+        if (Player.vaiatacar == false && vezplayer == true && Inimigo.inimigovaiatacar == false && Player.podeatacar == true && CameraTouch.tocounatela == false) //camera do player
+        {
+
+            float posCamX = playertransform.position.x;
+            float posCamY = playertransform.position.y;
+
+            if (cam.transform.position.x < LimiteCamEsq.position.x && playertransform.transform.position.x < LimiteCamEsq.position.x)
+            {
+                posCamX = LimiteCamEsq.position.x;
+            }
+            else if (cam.transform.position.x > LimiteCamDir.position.x && playertransform.transform.position.x > LimiteCamDir.position.x)
+            {
+                posCamX = LimiteCamDir.position.x;
+            }
+
+            if (cam.transform.position.y < LimiteCamBaixo.position.y && playertransform.transform.position.y < LimiteCamBaixo.position.y)
+            {
+                posCamY = LimiteCamBaixo.position.y;
+            }
+            else if (cam.transform.position.y > LimiteCamCima.position.y && playertransform.transform.position.y > LimiteCamCima.position.y)
+            {
+                posCamY = LimiteCamCima.position.y;
+            }
+            Vector3 posCam = new Vector3(posCamX, posCamY, cam.transform.position.z);
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position, posCam, velocidadeCam * Time.deltaTime);
+        }
+        //Debug.Log(vezinimigo);
+        //Debug.Log(Player.vaiatacar);
+        //Debug.Log(Inimigo.inimigovaiatacar);
+        //Debug.Log(Player.podeatacar);
+        if (vezinimigo == true && Player.vaiatacar == false && Inimigo.inimigovaiatacar == false && Player.podeatacar == false && CameraTouch.tocounatela == false) //camera inimigo
+        {
+            //Debug.Log("mudou de camera");
+            float posCamInimigoX = inimigoTransform.position.x;
+            float posCamInimigoY = inimigoTransform.position.y;
+
+            if (cam.transform.position.x < LimiteCamEsq.position.x && inimigoTransform.transform.position.x < LimiteCamEsq.position.x)
+            {
+                posCamInimigoX = LimiteCamEsq.position.x;
+            }
+            else if (cam.transform.position.x > LimiteCamDir.position.x && inimigoTransform.transform.position.x > LimiteCamDir.position.x)
+            {
+                posCamInimigoX = LimiteCamDir.position.x;
+            }
+
+            if (cam.transform.position.y < LimiteCamBaixo.position.y && inimigoTransform.transform.position.y < LimiteCamBaixo.position.y)
+            {
+                posCamInimigoY = LimiteCamBaixo.position.y;
+            }
+            else if (cam.transform.position.y > LimiteCamCima.position.y && inimigoTransform.transform.position.y > LimiteCamCima.position.y)
+            {
+                posCamInimigoY = LimiteCamCima.position.y;
+            }
+            Vector3 posInimigoCam = new Vector3(posCamInimigoX, posCamInimigoY, cam.transform.position.z);
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position, posInimigoCam, velocidadeCam * Time.deltaTime);
+        }
+
+        if (inimigoTirotransform != null)
+        {
+            if (Inimigo.inimigovaiatacar == true && Player.podeatacar == false && CameraTouch.tocounatela == false) //camera kunay
+            {
+
+                float posCamKunayX = inimigoTirotransform.position.x;
+                float posCamKunayY = inimigoTirotransform.position.y;
+
+                if (cam.transform.position.x < LimiteCamEsq.position.x && inimigoTirotransform.transform.position.x < LimiteCamEsq.position.x)
+                {
+                    posCamKunayX = LimiteCamEsq.position.x;
+                }
+                else if (cam.transform.position.x > LimiteCamDir.position.x && inimigoTirotransform.transform.position.x > LimiteCamDir.position.x)
+                {
+                    posCamKunayX = LimiteCamDir.position.x;
+                }
+
+                if (cam.transform.position.y < LimiteCamBaixo.position.y && inimigoTirotransform.transform.position.y < LimiteCamBaixo.position.y)
+                {
+                    posCamKunayY = LimiteCamBaixo.position.y;
+                }
+                else if (cam.transform.position.y > LimiteCamCima.position.y && inimigoTirotransform.transform.position.y > LimiteCamCima.position.y)
+                {
+                    posCamKunayY = LimiteCamCima.position.y;
+                }
+
+                Vector3 CamKunay = new Vector3(posCamKunayX, posCamKunayY, cam.transform.position.z);
+                cam.transform.position = Vector3.Lerp(cam.transform.position, CamKunay, velocidadeCam * Time.deltaTime);
+
+            }
+        }
+
+    }
+
+    public void Playsfx(AudioClip sfxclip, float volume)
+    {
+        sfxsource.PlayOneShot(sfxclip, volume);
+    }
+}
---
+++ 222/Assets/Scripts/Player.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private GameController GameController; //cria uma variavel GameController que vamos associar ao script depois no start
+
+    public Transform posicaotiro;
+    public GameObject tiro;
+
+    public float velocidade; //cria uma variavel publica chamada velocidade (float varia somente números de 0 até infinito)
+
+    private Animator PlayerAnimator; //lê o animator e da seu nome
+    private Rigidbody2D PlayerRb; //cria uma variavel privada chamada playerRb que vai ter as propriedas do rigidbody lida no unity
+
+    public bool olhaesquerdo; //cria uma variavel public de verdadeiro ou falso(bool) chamdo olhaesquerdo
+
+    public bool vaiatacar = false;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public BarraVida BarraVida;
+
+    private Atirar Atirar;
+    public Text angulotxt;
+    public float anguloarma = 20;
+
+    public bool apertou = false;
+    public float forcaataque;
+    public BarraVida barraforca;
+
+    public bool podeatacar = true;
+
+    public Transform iniciomira;
+    public Transform fimmira;
+    private Vector2 pontoinicio;
+    private Vector2 pontofim;
+    private Vector2 direcao;
+
+    public bool apertoucima;
+    public bool apertoubaixo;
+
+    private CameraTouch CameraTouch;
+
+    public float andatouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Atirar = FindObjectOfType(typeof(Atirar)) as Atirar;
+
+        PlayerAnimator = GetComponent<Animator>(); //inicializa o animator e associa ao nome PlayerAnimator
+        PlayerRb = GetComponent<Rigidbody2D>(); //inicializa e associa o rigidbody ao playerRb
+
+        GameController = FindObjectOfType(typeof(GameController)) as GameController; //procura o outro script chamado GameController e incializa
+        GameController.playertransform = this.transform; //vai pegar as informações do transform do player e associar ao playertransform do GameController
+
+        currentHealth = maxHealth;
+        BarraVida.SetMaxHealth(maxHealth);
+        barraforca.SetMaxHealth(100);
+
+        CameraTouch = FindObjectOfType(typeof(CameraTouch)) as CameraTouch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pontoinicio = iniciomira.position;
+        pontofim = fimmira.position;
+        direcao = (pontofim - pontoinicio).normalized;
+
+        if (CrossPlatformInputManager.GetButtonDown("Horizontal"))
+        {
+            //CameraTouch.tocounatela = false;
+        }
+
+        float anda = CrossPlatformInputManager.GetAxis("Horizontal"); //variavel chamada anda que recebe o aperto do teclado seta < ou > e faz o movimento Horizontal do personagem
+
+        if (anda > 0 && olhaesquerdo == true) //Se anda é maior que 0 e a caixa de marcar do unity for verdadeiro faz a função abaixo Flip
+        {
+            Flip(); //chama a função Flip criada            
+        }
+
+        if (anda < 0 && olhaesquerdo == false)
+        {
+            Flip();
+        }
+
+        andatouch = anda;
+
+        //if (CrossPlatformInputManager.GetButtonDown("Fire1")) //se aperta a tecla ctrl o personagem ataca
+        //{
+        //    PlayerAnimator.SetTrigger("ataca"); //faz a animação ataca
+        //    vaiatacar = true;
+        //}
+
+        if (CrossPlatformInputManager.GetButtonDown("botaocima")) //se aperta a tecla ctrl o personagem ataca
+        {
+            apertoucima = true;
+            GameController.Playsfx(GameController.sfxbotao, 0.5f);
+            //CameraTouch.tocounatela = false;
+        }
+
+        if (CrossPlatformInputManager.GetButtonUp("botaocima"))
+        {
+            apertoucima = false;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("botaobaixo")) //se aperta a tecla ctrl o personagem ataca
+        {
+            apertoubaixo = true;
+            GameController.Playsfx(GameController.sfxbotao, 0.5f);
+            //CameraTouch.tocounatela = false;
+        }
+
+        if (CrossPlatformInputManager.GetButtonUp("botaobaixo"))
+        {
+            apertoubaixo = false;
+        }
+
+        if (apertoucima && anguloarma <= 90)
+        {
+            anguloarma += 10 * Time.deltaTime;
+        }
+
+        if (apertoubaixo && anguloarma >= 15)
+        {
+            anguloarma -= 10 * Time.deltaTime;
+        }
+
+        angulotxt.text = anguloarma.ToString("F0"); //txtangulo do tipo texto recebe o anguloY convertido para texto se não da erro 
+
+        //iniciomira.eulerAngles = new Vector3(iniciomira.rotation.x, iniciomira.rotation.y, anguloarma);
+        if (olhaesquerdo == true)
+        {
+            //iniciomira.transform.localRotation = new Vector3(180, 0, 0);
+            //float mirarotaciona = iniciomira.transform.localRotation.x + 180;
+            //iniciomira.transform.localRotation.x = mirarotaciona;
+            //iniciomira.transform.localRotation = new Vector3(mirarotaciona, iniciomira.transform.localScale.y, iniciomira.transform.localScale.z);
+            iniciomira.eulerAngles = new Vector3(0, iniciomira.transform.localRotation.y, -anguloarma);
+
+            //iniciomira.transform.rotation = Quaternion.Euler(0, iniciomira.transform.localRotation.y, anguloarma);
+        }
+        else
+        {
+            iniciomira.eulerAngles = new Vector3(0, iniciomira.transform.localRotation.y, anguloarma);
+            //iniciomira.transform.rotation = Quaternion.Euler(0, iniciomira.transform.localRotation.y, anguloarma);
+        }
+
+        PlayerRb.velocity = new Vector2(anda * velocidade, PlayerRb.velocity.y); //acessa a velocidade do playerRb que vai receber o anda vezes a velocidade em X, e a velocidade em Y
+        PlayerAnimator.SetInteger("anda", (int)anda); //faz a animação anda, mas converte para inteiro devido no unity ser configurado como int e na programação é float
+
+        if (currentHealth <= 0)
+        {
+            PlayerAnimator.SetTrigger("morrendo");
+            transform.gameObject.SetActive(false);
+            GameController.currentstate = gamestate.FIMJOGO;
+            GameController.painelfim.SetActive(true);
+            GameController.pausatempo = true;
+        }
+
+        if (apertou == true && forcaataque < 100 && podeatacar == true)
+        {
+            forcaataque += 20 * Time.deltaTime;
+        }
+
+        barraforca.SetHealth(Mathf.RoundToInt(forcaataque));
+
+
+    }
+
+    void Flip() //criamos uma função que não tem no unity chamada Flip
+    {
+        //anguloarma *= -1;
+        olhaesquerdo = !olhaesquerdo; //olhaesquerdo recebe a diferença de olhaesquerdo
+        //float x = transform.localScale.x * -1; //cria uma variavel x que recebe no unity em transform depois Scale no x e multiplica por menos um (faz o giro do personagem em x)
+        //transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z); //acessa o transforma no Scale que vai receber um novo vector x que criamos
+        Vector3 tempScale = transform.localScale;
+        tempScale.x *= -1;
+        transform.localScale = tempScale;
+
+        /*if (olhaesquerdo == true)
+        {
+            //iniciomira.transform.localRotation = new Vector3(180, 0, 0);
+            //float mirarotaciona = iniciomira.transform.localRotation.x + 180;
+            //iniciomira.transform.localRotation.x = mirarotaciona;
+            //iniciomira.transform.localRotation = new Vector3(mirarotaciona, iniciomira.transform.localScale.y, iniciomira.transform.localScale.z);
+            iniciomira.transform.localRotation = Quaternion.Euler(180, iniciomira.transform.localRotation.y, iniciomira.transform.localRotation.z);
+        }
+        else
+        {
+            iniciomira.transform.localRotation = Quaternion.Euler(0, iniciomira.transform.localRotation.y, iniciomira.transform.localRotation.z);
+        }
+        */
+
+
+        /*if (olhaesquerdo)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 180, transform.rotation.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+        }
+        */
+    }
+
+    void Atira()
+    {
+        if (podeatacar == true)
+        {
+            GameObject tiroPreFab = Instantiate(tiro, posicaotiro.position, tiro.transform.localRotation);
+
+            if (olhaesquerdo)
+            {
+                tiroPreFab.GetComponent<Rigidbody2D>().AddForce(-iniciomira.transform.right * forcaataque * 15);
+            }
+            else
+            {
+                tiroPreFab.GetComponent<Rigidbody2D>().AddForce(iniciomira.transform.right * forcaataque * 15);
+            }
+        }
+
+    }
+
+    void Tocapasso()
+    {
+        GameController.Playsfx(GameController.sfxpasso[Random.Range(0, GameController.sfxpasso.Length)], 0.3f);
+    }
+
+    public void Apertouataque()
+    {
+        if (podeatacar == true)
+        {
+            apertou = true;
+            GameController.pausatempo = true;
+        }
+
+    }
+
+    public void Soltouataque()
+    {
+        if (podeatacar == true)
+        {
+            PlayerAnimator.SetTrigger("ataca"); //faz a animação ataca
+            vaiatacar = true;
+            apertou = false;
+
+            CameraTouch.tocounatela = false;
+        }
+    }
+}
